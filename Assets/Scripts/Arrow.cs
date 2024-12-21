64cd642fc8e7240eea5cc258abcb16fd8c31ecf2
Hellagr <alehkushniarou@gmail.com>
2024-12-21 15:46:50 +0300

    [RCHR24] Arrow

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -2,54 +2,38 @@ using UnityEngine;
 
 public class Arrow : MonoBehaviour
 {
-    [SerializeField] PlayerInput movePlayerInput;
     [SerializeField] float speed = 3.0f;
-    [SerializeField] float radius = 5f;
     [SerializeField] public float damage { get; private set; } = 1f;
-    Vector3 currentposition;
-    Vector3 enemyPosition = Vector3.zero;
-    LayerMask enemyMask;
+    Rigidbody rb;
 
     void Start()
     {
-        enemyMask = LayerMask.NameToLayer("Enemy");
+        rb = GetComponent<Rigidbody>();
     }
 
-    void Update()
+    public void Launch<T>(T data)
     {
-        if (movePlayerInput.MoveInput == Vector2.zero)
+        Vector3 point = Vector3.zero;
+
+        if (data is Transform transformData)
         {
-            currentposition = transform.position;
-
-            Collider[] enemyAround = Physics.OverlapSphere(transform.position, radius, enemyMask);
-
-            float closestDistance = Mathf.Infinity;
-
-            foreach (Collider enemy in enemyAround)
-            {
-                float distance = Vector3.Distance(currentposition, enemy.transform.position);
-                if (distance < closestDistance)
-                {
-                    closestDistance = distance;
-                    enemyPosition = enemy.transform.position;
-                }
-            }
-            Launch(speed, enemyPosition);
+            point = transformData.position;
         }
-    }
+        else if (data is Vector3 vector3Data)
+        {
+            point = vector3Data;
+        }
+        else
+        {
+            Debug.LogError("Unknown type of data");
+        }
 
-    public void Launch(float speed, Vector3 enemyPoint)
-    {
-        transform.LookAt(enemyPoint);
-        transform.Translate(Vector3.forward * speed);
+        rb.linearVelocity = point * speed;
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.layer == enemyMask)
-        {
-            Destroy(gameObject);
-        }
+        Destroy(gameObject);
     }
 }
 
